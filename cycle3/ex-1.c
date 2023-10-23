f00760d60673875f4c1d22714eef99d000872b3d
fbn776 <fbn776@gmail.com>
2023-10-23 17:45:54 +0530

    infix to postfix v-1

--- cycle3/ex-1.c
+++ cycle3/ex-1.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
 
-#define ERROR "ERROR"
-#define SIZE 100
-#define POP_ERROR '~'
-
-void Push(char S[], int *top, char item) {
-	if (*top == SIZE)
-		return;
-
-	S[(*top)++] = item;
+//// STACK OPERATIONS
+void Push(char s[], int *top, char item) {
+	s[++(*top)] = item;
 }
 
-char Pop(char S[], int *top) {
+char Pop(char s[], int *top) {
 	if (*top == -1)
-		return POP_ERROR;
-	else {
-		return S[(*top)--];
-	}
+		return '\0';
+
+	return s[(*top)--];
 }
 
-int getPrecedence(char item) {
-	switch (item) {
+/**Checks if a given character is an operand**/
+int isOperand(char ch) {
+	return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
+}
+
+/**Checks if a given character is an operator**/
+int isOperator(char ch) {
+	return (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^');
+}
+
+/**Gets the precedence of an operator**/
+int getPrecedence(char ch) {
+	switch (ch) {
 		case '+':
 		case '-':
 			return 1;
@@ -30,79 +34,56 @@ int getPrecedence(char item) {
 		case '^':
 			return 3;
 		default:
-			return -1;
-	}
-}
-
-//Checks if a character is an alphabet
-int isAlpha(char ch) {
-	return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
-}
-
-//Checks if a character is an operator or not
-int isOperator(char ch) {
-	return (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^');
-}
-
-//Checks if ch is a bracket; if left bracket -1, if right bracket 1, else 0
-int isBrace(char ch) {
-	return ch == '(' ? -1 : (ch == ')' ? 1 : 0);
-}
-
-void convertToPostfix(char input[], int len, char result[]) {
-	char Stack[SIZE], item = '(';
-	int top = -1, i, k = 0;
-
-
-	//Push an initial left parenthesis to the stack
-	Push(Stack, &top, '(');
-
-	for (i = 0; i < len; i++) {
-		char curr = input[i];
-
-		printf("Char is %c\n", curr);
-
-		if (curr == ' ' || curr == '\t')
-			continue;
-
-		if (isAlpha(curr) == 1)
-			result[k++] = curr;
-		else if (isOperator(curr) == 1) {
-			if (top == -1 || getPrecedence(Stack[top]) < getPrecedence(curr)) {
-				Push(Stack, &top, curr);
-			} else {
-				while (top > -1 && getPrecedence(Stack[top]) >= getPrecedence(curr)) {
-					result[k++] = Pop(Stack, &top);
-				}
-			}
-		}
-//		else if (isBrace(curr)) {
-//			if(isBrace(curr) == 1) {
-//				while(top > -1 && Stack[top] != '(') {
-//					result[k++] = Pop(Stack, &top);
-//				}
-//			}
-//		} else
-			break;
-	}
-
-	while(top > -1) {
-		item = Pop(Stack, &top);
-
-		result[k++] = item;
+			return 0;
 	}
 }
 
 int main() {
-	char input[100], output[100];
-	int len = 0;
+	char Stack[100], Q[100] = "a^b^c", P[100], curr;
+	int top = -1, len = 0, i, j = 0;
 
-	printf("Enter infix expression: ");
-	scanf("%[^\n]", input);
+//	printf("Enter Expression: ");
+//	scanf("%[^\n]", Q);
 
-	for (; input[len] != '\0'; len++) ;
+	//Find length
+	for (; Q[len] != '\0'; len++);
 
-	convertToPostfix(input, len, output);
+	//Add initial brackets
+	Push(Stack, &top, '(');
+	Q[len++] = ')';
+	Q[len] = '\0';
 
-	printf("Result is: %s\n", output);
+	for (i = 0; Q[i] != '\0'; i++) {
+		curr = Q[i];
+		if (curr == ' ' || curr == '\t')
+			continue;
+
+
+		if (isOperand(curr)) {
+			P[j++] = curr;
+		} else if (curr == '(') {
+			Push(Stack, &top, curr);
+		} else if (isOperator(curr)) {
+
+			while (top > -1 && getPrecedence(curr) <= getPrecedence(Stack[top])) {
+				char item = Pop(Stack, &top);
+				P[j++] = item;
+			}
+			Push(Stack, &top, curr);
+
+		} else if (curr == ')') {
+			while (top > -1 && Stack[top] != '(') {
+				char item = Pop(Stack, &top);
+				P[j++] = item;
+			}
+			Pop(Stack, &top);
+		} else {
+			printf("Error is expression\n");
+			break;
+		}
+	}
+	P[j] = '\0';
+
+	printf("P: %s\n", P);
+
 }
