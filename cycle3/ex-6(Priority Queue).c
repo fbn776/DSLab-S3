7468a42888368e7eb48aad9efbb314f26a1fcacd
fbn776 <fbn776@gmail.com>
2024-02-11 16:59:59 +0530

    Fixed priority queue

--- cycle3/ex-6(Priority Queue).c
+++ cycle3/ex-6(Priority Queue).c
@@ -1,87 +1,79 @@
 #include <stdio.h>
+#define SIZE 10
 
-#define MAX_SIZE 100
-
-/**More priority means higher priority number*/
 typedef struct {
 	int data;
-	int priority;
+	int prio;
 } Item;
 
-typedef struct {
-	Item Queue[MAX_SIZE];
-	int size;
-} PQueue;
+void Enqueue(Item Q[], int *size, Item item) {
+	if(*size == SIZE)
+		printf("Queue full\n");
+	else {
+		Q[(*size)++] = item;
+	}
+}
 
-void Enqueue(PQueue *pq, Item item) {
-	if (pq->size >= MAX_SIZE) {
-		printf("Priority Queue is full, cannot enqueue.\n");
+void Dequeue(Item Q[], int *size) {
+	if(*size == 0) {
+		printf("Queue empty\n");
+	} else {
+		int i, maxPrio = 0, maxAt = 0;
+		Item item;
+		for(i = 0; i < *size; i++) {
+			if(Q[i].prio > maxPrio) {
+				maxPrio = Q[i].prio;
+				maxAt = i;
+			}
+		}
+
+		item = Q[maxAt];
+
+		// Shift the elements to left
+		for(i = maxAt; i < *size - 1; i++) {
+			Q[i] = Q[i+1];
+		}
+		(*size)--;
+
+		printf("Data -> %d\nPrio -> %d\n", item.data, item.prio);
+	}
+}
+
+void Display(Item Q[], int size) {
+	int i;
+	if(size == 0) {
+		printf("Queue empty\n");
 		return;
 	}
 
-	int index = pq->size;
-
-	while (index > 0 && item.priority > pq->Queue[index - 1].priority) {
-		pq->Queue[index] = pq->Queue[index - 1];
-		index--;
-	}
-
-	pq->Queue[index] = item;
-	pq->size++;
-}
-
-Item Dequeue(PQueue *pq) {
-	Item i;
-	if (pq->size <= 0) {
-		printf("Priority Queue is empty, cannot dequeue.\n");
-		i.priority = -1;
-		return i;
-	}
-
-	i = pq->Queue[pq->size - 1];
-	pq->size--;
-
-	return i;
-}
-
-
-void Display(PQueue pq) {
-	int i;
-	for (i = 0; i < pq.size; i++)
-		printf("Data: %d --- Priority: %d\n", pq.Queue[i].data, pq.Queue[i].priority);
-
-	printf("\n");
+	for(i = 0; i < size; i++)
+		printf("Data -> %d\nPrio -> %d\n\n", Q[i].data, Q[i].prio);
 }
 
 int main() {
-	char cnt = 'Y';
-	int choice;
-	PQueue pq;
-	Item item;
-	pq.size = 0;
+	Item Q[SIZE];
+	int size = 0, choice;
 
-
-	printf("===Choices===\n1 - Enqueue\n2 - Dequeue\n3 - Display\n4 - Exit\n");
-
-	while (cnt == 'Y') {
-		printf("\nEnter choice: ");
+	printf("Choices;\n1 - Enqueue\n2 - Dequeue\n3 - Display\n4 - Exit\n");
+	while(1) {
+		printf("> ");
 		scanf(" %d", &choice);
-		switch (choice) {
+
+		switch(choice) {
 			case 1:
-				printf("Enter data and priority: ");
-				scanf(" %d %d", &item.data, &item.priority);
-				Enqueue(&pq, item);
-				break;
+				Item item;
+				printf("Enter data, priority: ");
+				scanf(" %d %d", &(item.data), &(item.prio));
+				Enqueue(Q, &size, item);
+			break;
 			case 2:
-				item = Dequeue(&pq);
-				item.priority == -1 ?: printf("Data: %d --- Priority: %d\n", item.data, item.priority);
-				break;
+				Dequeue(Q, &size);
+			break;
 			case 3:
-				Display(pq);
-				break;
+				Display(Q, size);
+			break;
 			default:
-				cnt = 'N';
+				return 0;
 		}
 	}
 }
-
